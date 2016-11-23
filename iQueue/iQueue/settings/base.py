032d0cd21819fda60897c11887dec7887a2e1d25
Yadnyesh <yaduonline@gmail.com>
2016-11-23 16:51:26 +0530

    Adding dj-database-url

--- iQueue/iQueue/settings/base.py
+++ iQueue/iQueue/settings/base.py
@@ -11,10 +11,19 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-
+from os import environ
+from django.core.exceptions import ImproperlyConfigured
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+def get_env_setting(setting):
+    """ Get the environment setting or return exception """
+    try:
+        return environ[setting]
+    except KeyError:
+        error_msg = "Set the %s env variable" % setting
+        raise ImproperlyConfigured(error_msg)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
--- iQueue/iQueue/settings/stage.py
+++ iQueue/iQueue/settings/stage.py
@@ -12,3 +12,19 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+########## AWS CONFIGURATIOM
+AWS_ACCESS_KEY_ID = get_env_setting('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_env_setting('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = get_env_setting('AWS_STORAGE_BUCKET_NAME')
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = S3_URL
+AWS_QUERYSTRING_AUTH = False
+########## END AWS CONFIGURATION
+
+########## HEROKU DATABASE CONFIGURATION
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
+########## END HEROKU DATABASE CONFIGURATION
